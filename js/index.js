235ef90c4bc1957262465297eec4fa23d02fb281
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2022-07-12 22:26:00 +0530

    dir changes

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    location.replace("main.html");
+    location.replace("../main.html");
   }
 });
 
