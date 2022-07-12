97f4626198eca43134565e7c07cb389c93cbb509
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-07-12 18:46:05 +0530

    ss

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    location.replace("login.html");
+    location.replace("main.html");
   }
 });
 
