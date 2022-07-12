dc1a14acc72d9e7ecb3264c2f31009b2ecdde1bc
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-07-12 18:28:56 +0530

    h

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged((user) => {
   if (!user) {
-    location.replace("login.html");
+    location.replace("../login.html");
   } else {
     document.getElementById("user").innerHTML =
       "Hello " + (user.displayName ? user.displayName : user.email);
--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    location.replace("index.html");
+    location.replace("../index.html");
   }
 });
 
